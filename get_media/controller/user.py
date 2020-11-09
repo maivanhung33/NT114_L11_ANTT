b94bd0746cc9c0586f91cfda4106ded7c2b86d00
maihung303 <maihung303@bitbucket.org>
2020-11-09 16:38:50 +0700

    update sth

--- get_media/controller/user.py
+++ get_media/controller/user.py
@@ -5,6 +5,6 @@ from get_media.service import user
 urlpatterns = [
     path('register', user.register, name='register'),
     path('token', user.token, name='token'),
-    path('confirm-otp', user.confirm_otp, name='confirm_otp'),
+    path('verify', user.confirm_otp, name='verify'),
 
 ]
