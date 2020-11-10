202ff1dbc1524d9c6f9a890a4fe2214d6eb78dd9
maihung303 <maihung303@bitbucket.org>
2020-11-10 14:53:03 +0700

    update sth

--- get_media/service/user.py
+++ get_media/service/user.py
@@ -24,12 +24,12 @@ SERVICE_ID = os.environ.get('SERVICE_ID') or 'VAbc7be24b5576704783b92dd68283da72
 def register(request):
     form = UserRegister(request.POST)
     if not form.is_valid():
-        return JsonResponse(status=400, data=dict(message="Bad Request"))
+        return JsonResponse(status=400, data=dict(message='Bad Request'))
 
-    col = DB["user"]
-    is_exits = col.find_one({"phone": form.cleaned_data['phone'], "verified": True})
+    col = DB['user']
+    is_exits = col.find_one({'phone': form.cleaned_data['phone'], 'verified': True})
     if is_exits:
-        return JsonResponse(status=422, data=dict(message="User name has exits"))
+        return JsonResponse(status=422, data=dict(message='User name has exits'))
     new_user = User(phone=form.cleaned_data['phone'],
                     password=bcrypt.hashpw(form.cleaned_data['password'].encode(), bcrypt.gensalt()).decode(),
                     lastname=form.cleaned_data['lastname'],
@@ -37,12 +37,15 @@ def register(request):
                     birthday=form.cleaned_data['birthday'],
                     favorites=[])
 
-    col.insert_one(new_user.__dict__)
+    col.update(
+        {'phone': form.cleaned_data['phone']},
+        {'$set': new_user.__dict__},
+        upsert=True)
 
     send_otp(form.cleaned_data['phone'])
 
     return JsonResponse(status=201,
-                        data={"status": "success", "message": ""})
+                        data={'status': 'success', 'message': ''})
 
 
 @api_view(['POST'])
@@ -55,7 +58,7 @@ def token(request):
     else:
         form = UserRefresh(request.POST)
         if not form.is_valid():
-            return JsonResponse(status=400, data=dict(message="Bad Request"))
+            return JsonResponse(status=400, data=dict(message='Bad Request'))
         if form.cleaned_data['grant_type'] != 'refresh_token':
             return JsonResponse(status=400, data=dict(message='Grant type is not valid'))
         return refresh(form)
@@ -65,60 +68,60 @@ def token(request):
 def reset_password(request):
     form = ResetPassword(request.POST)
     if not form.is_valid():
-        return JsonResponse(status=400, data=dict(message="Bad Request"))
-    col = DB["user"]
-    user = col.find_one({"phone": form.cleaned_data['phone'], "verified": True})
+        return JsonResponse(status=400, data=dict(message='Bad Request'))
+    col = DB['user']
+    user = col.find_one({'phone': form.cleaned_data['phone'], 'verified': True})
     if not user:
-        return JsonResponse(status=404, data=dict(message="User not exits"))
+        return JsonResponse(status=404, data=dict(message='User not exits'))
 
     send_otp(form.cleaned_data['phone'])
 
     return JsonResponse(status=200,
-                        data={"status": "pending", "message": "waiting confirm otp"})
+                        data={'status': 'pending', 'message': 'waiting confirm otp'})
 
 
 @api_view(['POST'])
 def verify_opt_register(request):
     form = VerifyOtpRegister(request.POST)
     if not form.is_valid():
-        return JsonResponse(status=400, data=dict(message="Bad Request"))
-    col = DB["user"]
-    user = col.find_one({"phone": form.cleaned_data['phone'], 'verified': False})
+        return JsonResponse(status=400, data=dict(message='Bad Request'))
+    col = DB['user']
+    user = col.find_one({'phone': form.cleaned_data['phone'], 'verified': False})
     if not user:
-        return JsonResponse(status=404, data=dict(message="User not exits or Has verified"))
+        return JsonResponse(status=404, data=dict(message='User not exits or Has verified'))
 
     if verify_otp(phone=form.cleaned_data['phone'], otp=form.cleaned_data['otp']) == 'approved':
-        update_verified = {"$set": {"verified": True}}
-        col.update_one({"phone": form.cleaned_data['phone']}, update_verified)
-        return JsonResponse(status=200, data={"status": "success", "message": ""})
-    return JsonResponse(status=404, data={"status": "fail", "message": "Otp incorrect"})
+        update_verified = {'$set': {'verified': True}}
+        col.update_one({'phone': form.cleaned_data['phone']}, update_verified)
+        return JsonResponse(status=200, data={'status': 'success', 'message': ''})
+    return JsonResponse(status=404, data={'status': 'fail', 'message': 'Otp incorrect'})
 
 
 @api_view(['POST'])
 def verify_opt_reset_password(request):
     form = VerifyOtpResetPassword(request.POST)
     if not form.is_valid():
-        return JsonResponse(status=400, data=dict(message="Bad Request"))
-    col = DB["user"]
-    user = col.find_one({"phone": form.cleaned_data['phone'], 'verified': True})
+        return JsonResponse(status=400, data=dict(message='Bad Request'))
+    col = DB['user']
+    user = col.find_one({'phone': form.cleaned_data['phone'], 'verified': True})
     if not user:
-        return JsonResponse(status=404, data=dict(message="User not exits"))
+        return JsonResponse(status=404, data=dict(message='User not exits'))
 
     if verify_otp(phone=form.cleaned_data['phone'], otp=form.cleaned_data['otp']) == 'approved':
         update_password = {
-            "$set": {"password": bcrypt.hashpw(form.cleaned_data['password'].encode(), bcrypt.gensalt()).decode()}}
-        col.update_one({"phone": form.cleaned_data['phone']}, update_password)
-        return JsonResponse(status=200, data={"status": "success", "message": "Password has been updated"})
-    return JsonResponse(status=404, data={"status": "fail", "message": "Otp incorrect"})
+            '$set': {'password': bcrypt.hashpw(form.cleaned_data['password'].encode(), bcrypt.gensalt()).decode()}}
+        col.update_one({'phone': form.cleaned_data['phone']}, update_password)
+        return JsonResponse(status=200, data={'status': 'success', 'message': 'Password has been updated'})
+    return JsonResponse(status=404, data={'status': 'fail', 'message': 'Otp incorrect'})
 
 
 def login(data):
-    col = DB["user"]
-    user = col.find_one({"phone": data.cleaned_data['phone'], "verified": True})
+    col = DB['user']
+    user = col.find_one({'phone': data.cleaned_data['phone'], 'verified': True})
     if not user:
-        return JsonResponse(status=404, data=dict(message="User not exits"))
+        return JsonResponse(status=404, data=dict(message='User not exits'))
     if not bcrypt.checkpw(data.cleaned_data['password'].encode(), user['password'].encode()):
-        return JsonResponse(status=401, data=dict(message="Password is not correct"))
+        return JsonResponse(status=401, data=dict(message='Password is not correct'))
 
     response = dict(accessToken=auth.generate_access_token(phone=user['phone'],
                                                            email=user['email']).decode(),
@@ -134,8 +137,8 @@ def refresh(data):
     user = auth.check_refresh_token(data.cleaned_data['refresh_token'])
     if user in [0, -1]:
         return JsonResponse(status=400, data={'message': 'Refresh token is not correct or expired'})
-    col = DB["user"]
-    user = col.find_one({"password": user.password})
+    col = DB['user']
+    user = col.find_one({'password': user.password})
     if not user:
         return JsonResponse(status=400, data={'message': 'Can not refresh token'})
     response = dict(accessToken=auth.generate_access_token(phone=user['phone'],
@@ -153,7 +156,7 @@ def handle_uploaded_file(f, username, extension):
 
 
 def send_otp(phone):
-    phone_send_otp = phone.replace("0", "+84")
+    phone_send_otp = phone.replace('0', '+84')
     client = Client(ACCOUNT_SID, AUTH_TOKEN)
     return client.verify \
         .services(SERVICE_ID) \
@@ -162,7 +165,7 @@ def send_otp(phone):
 
 
 def verify_otp(phone, otp):
-    phone_send_otp = phone.replace("0", "+84")
+    phone_send_otp = phone.replace('0', '+84')
     client = Client(ACCOUNT_SID, AUTH_TOKEN)
     verification_check = client.verify \
         .services(SERVICE_ID) \
@@ -171,8 +174,8 @@ def verify_otp(phone, otp):
     return verification_check.status
 
 # def update():
-#     avatar_type = form.cleaned_data["avatar"].content_type
+#     avatar_type = form.cleaned_data['avatar'].content_type
 #     if avatar_type not in ['image/jpeg', 'image/png']:
-#         return JsonResponse(status=400, data=dict(message="Avatar must be JPEG or PNG"))
+#         return JsonResponse(status=400, data=dict(message='Avatar must be JPEG or PNG'))
 #     avatar_extension = avatar_type.split('/')[1]
 #     avatar_url = handle_uploaded_file(form.cleaned_data['avatar'], form.cleaned_data['username'], avatar_extension)
