cc2c6f420e2915c17b5729d9e844b6fed80e9e9e
maihung303 <maihung303@bitbucket.org>
2020-11-16 16:41:43 +0700

    update facebook

--- get_media/module/facebook.py
+++ get_media/module/facebook.py
@@ -38,31 +38,23 @@ class FaceBook:
                 return {'owner': None, 'data': []}
             if video_id == '':
                 return {'owner': None, 'data': []}
+            video = FaceBook.get_video_info(video_id)
             item = dict(
-                url=self.__get_url_video(video_id),
-                thumbnail=None,
-                publishTime=None,
-                playCount=None
-            )
+                url=FaceBook.get_url_video(video_id),
+                thumbnail=video['thumbnail'],
+                title=video['title'])
             return {'owner': None, 'data': [item]}
 
     def __get_page_id(self):
-        url = "https://www.facebook.com/ajax/bulk-route-definitions/"
-
-        payload = 'route_urls[0]=/{}&fb_dtsg=AQG8efRGZiDH%3AAQEEcyya2P4u'.format(self.__page_name)
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Cookie': 'c_user=100006040201647; xs=24%3AILjNxnOC6pdzeQ%3A2%3A1605231199%3A16304%3A6190%3A%3AAcUM7Y2oyTY0js50qnx2470-fzlhfBYqp4AtcVCMMQ; fr=0hnKe9SVLKEFxvJpl.AWWaOx7sXjL1xwzfYfHQC3joFCM.BfftX_.5K.AAA.0.0.BfsVTH.AWVU_ESfuOk'
-        }
-
-        response = requests.request("POST", url, headers=headers, data=payload)
-        try:
-            id = json.loads(response.text.replace('for (;;);', ''))
-            id = id['payload']['payloads']['/' + self.__page_name]['result']['exports']['hostableView']['props'][
-                'pageID']
-        except Exception as e:
+        page_html = requests.get(self.__url).text
+        if page_html.find('\"fb://page/?id=') == -1:
             return ''
-        return id
+        page_id = page_html.split('"fb://page/?id=')[1].split("\"")[0]
+        self.__page_avatar = page_html \
+            .split('\"PagesProfilePictureEditMenu\"')[1] \
+            .split('\"uri\":\"')[1].split('\"')[0] \
+            .replace('\\', '')
+        return page_id
 
     def __get_latest_videos(self, page_id):
         url = "https://www.facebook.com/api/graphql/"
@@ -82,33 +74,49 @@ class FaceBook:
         }
 
         response = requests.request("POST", url, headers=headers, data=payload)
-
         response = response.text.replace('\r\n', ',').replace(' ', '').replace('\n', '').replace('\r', '').replace('$',
                                                                                                                    '')
         response = '{\"content\":[' + response + ']}'
         json_data = json.loads(response)
+
         list_latest_videos = []
         for item in json_data['content']:
             if 'label' in item.keys() \
                     and item['label'] == 'CometSinglePageChannelTabRootQuerydeferCometChannelTabLatestVideosCard_page':
                 list_latest_videos = item['data']['latest_videos']['edges']
                 break
-        response = dict(
-            owner={'name': self.__page_name},
-            data=[]
-        )
+
+        owner = FaceBook.get_page_info(page_id)
+        owner['avatar'] = self.__page_avatar
+        response = dict(owner=owner, data=[])
         for item in list_latest_videos:
-            video_url = self.__get_url_video(item['node']['channel_tab_thumbnail_renderer']['video']['id'])
-            video = dict(
-                url=video_url,
-                thumbnail=item['node']['channel_tab_thumbnail_renderer']['video']['image']['uri'],
-                publishTime=item['node']['channel_tab_thumbnail_renderer']['video']['publish_time'],
-                playCount=item['node']['channel_tab_thumbnail_renderer']['video']['play_count']
-            )
+            video_url = FaceBook.get_url_video(item['node']['channel_tab_thumbnail_renderer']['video']['id'])
+            video = dict(url=video_url,
+                         title=FaceBook.get_video_info(item['node']['channel_tab_thumbnail_renderer']['video']['id'])[
+                             'title'],
+                         thumbnail=item['node']['channel_tab_thumbnail_renderer']['video']['image']['uri'],
+                         publishTime=item['node']['channel_tab_thumbnail_renderer']['video']['publish_time'],
+                         playCount=item['node']['channel_tab_thumbnail_renderer']['video']['play_count'])
             response['data'].append(video)
+
         return response
 
-    def __get_url_video(self, video_id):
+    @staticmethod
+    def get_video_info(video_id):
+        url = "https://www.facebook.com/api/graphql/"
+
+        payload = 'fb_dtsg=AQFAz9HPLA8b:AQExNUApkJQU&variables={"id":\"' + video_id + '\"}&doc_id=3505988849454782'
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'}
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        return dict(thumbnail=response.json()['data']['video']['image']['uri'],
+                    owner_id=response.json()['data']['video']['owner']['id'],
+                    title=response.json()['data']['video']['title']['text'])
+
+    @staticmethod
+    def get_url_video(video_id):
         url = "https://www.facebook.com/api/graphql/"
 
         payload = 'fb_dtsg=AQFAz9HPLA8b:AQExNUApkJQU&variables={"id":' \
@@ -116,13 +124,43 @@ class FaceBook:
                   + '}&doc_id=4650955718313204'
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Cookie': 'c_user=100006040201647; spin=r.1002938977_b.trunk_t.1604846167_s.1_v.2_;'
-                      + ' xs=43%3ArXUM7Zy8rJpH9Q%3A2%3A1587656106%3A16304%3A6190%3A%3AAcUWEzfBAdJBVdGiDAOb__-QTkD25AvQrT4VDpVPx9nr; fr=0hnKe9SVLKEFxvJpl.AWX6qFRbxptoYcIHn2QNHFmnYJU.BfftX_.VL.AAA.0.0.BfqPZZ.AWUnMSp93VA'
+        }
+
+        request_response = requests.request("POST", url, headers=headers, data=payload)
+        return request_response.json()['data']['video']['playable_url']
+
+    @staticmethod
+    def get_page_info(page_id):
+        url = "https://www.facebook.com/api/graphql/"
+
+        payload = 'variables={"pageID":\"' + page_id + '\"}&doc_id=1766735040109284'
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
         }
 
         response = requests.request("POST", url, headers=headers, data=payload)
-        return response.json()['data']['video']['playable_url']
+        follow = response.json()['data']['page']['video_page_social_context']['sentence']['text']
 
+        url = "https://www.facebook.com/api/graphql/"
+
+        payload = 'variables={\"pageID\":\"' + page_id + '\",\"showContextualColor\":false}&doc_id=2915378275224907'
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        name = response.json()['data']['page']['name']
+        websites = response.json()['data']['page']['websites']
+        about = response.json()['data']['page']['about']['text']
+        page_video = response.json()['data']['page']['page_video']['page_video_count']
+
+        return dict(
+            name=name, follow=follow, websites=websites, about=about, count_video=page_video
+        )
+
+# 194149144034882
+
+# print(FaceBook.get_page_info('194149144034882'))
+# print(requests.get('https://www.facebook.com/trollbongda').text)
+# page_html = requests.get('https://www.facebook.com/trollbongda').text
 # _url = 'https://fb.watch/1oIoNues8H/'
 # _url = 'https://www.facebook.com/nguoc.confessions'
 # fb = FaceBook(_url)
