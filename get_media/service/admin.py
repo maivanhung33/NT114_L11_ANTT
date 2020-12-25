6797b52b7a7e9c73aacc572a1ae2bccdff9fff64
maihung303 <maihung303@bitbucket.org>
2020-12-25 14:47:55 +0700

    update log

--- get_media/service/admin.py
+++ get_media/service/admin.py
@@ -331,5 +331,5 @@ def write_log(data, action, user=None):
     col = DB['log']
     data['time'] = int(datetime.now().timestamp())
     data['type'] = action
-    data['user'] = user.__dict__ if user is not None and not isinstance(user, dict) else None
+    data['user'] = user.__dict__ if user is not None and not isinstance(user, dict) else user
     col.insert_one(data)
